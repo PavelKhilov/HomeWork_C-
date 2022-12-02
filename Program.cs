f298527cb32b811495d67e7321ef9fc2748b5298
Pavel <khilov.pavel@eyandex.ru>
2022-12-02 19:51:48 +0300

    Exercise 6

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ else
 */
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
 Console.WriteLine("Input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number: ");
@@ -32,5 +33,14 @@ if(num3 > max)
     max = num3;
 
 Console.WriteLine("Maximum number is: " + max);
+*/
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+    Console.WriteLine("Even number");
+else
+    Console.WriteLine("Odd number");
