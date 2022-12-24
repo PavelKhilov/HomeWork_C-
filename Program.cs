4c64dd0ff497f758a416fb2882e2edcc6c799bfd
Pavel <khilov.pavel@eyandex.ru>
2022-12-24 14:40:26 +0300

    Exersise 52

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ Show2dArrayDouble(arrayDouble);
 */
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
-
+/*
 int[,] CreateRandom2dArray()
 {
     Console.Write("Input a number of rows = ");
@@ -84,5 +84,61 @@ if(FundElement2dArray(newArray, row, column))
     Console.WriteLine($"The element of array on position {row}:{column} is number = {newArray[row -1 ,column - 1]}.");
 else
     Console.WriteLine($"The sise of find row = {row} or column = {column} is to big for this array!");
+*/
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows = ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns = ");
+    int columns = Convert.ToInt32(Console.ReadLine());    
+    Console.Write("Input a minimum possible value = ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum possible value = ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write(array[i] + "\t");
+    Console.WriteLine();
+}
+
+int[] AverageColumns2dArray (int[,] array)
+{
+    int[] averageCulumns = new int[array.GetLength(1)];
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            averageCulumns[j] += array[i,j];
+        }
+    }
+    return averageCulumns;
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Console.WriteLine();
+int[] avgArray = AverageColumns2dArray(myArray);
+ShowArray(avgArray);
